d19934a0814c00d87b0a016c8bb3965a5ef685d5
Luiz Felipe Moreira <felipecbm@gmail.com>
2017-07-08 23:58:00 -0300

    implementando o assignment 4

---
+++ rest-server-passport/routes/favoritesRouter.js
@@ -0,0 +1,176 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Verify = require('./verify');
+
+var Favorites = require('../models/favorites');
+
+var favoritesRouter = express.Router();
+favoritesRouter.use(bodyParser.json());
+
+favoritesRouter.route('/')
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Favorites.find({})
+        .populate('postedBy')
+        .populate('dishes')
+        .exec(function (err, dish) {
+          if (err) throw err;
+          res.json(dish);
+    });
+})
+
+.post(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Favorites.findOne({postedBy: req.decoded._doc._id})
+      .exec(function (err,favorites) {
+        if (err) throw err;
+
+        if (favorites === null) {
+          var favoritos = {postedBy: '', dishes: []};
+          favoritos.postedBy = req.decoded._doc._id;
+          favoritos.dishes.push(req.body._id)
+
+          Favorites.create(favoritos, function (err, favorites) {
+            if (err) throw err;
+            console.log('Favorites created!');
+            res.writeHead(200, {
+              'Content-Type': 'text/plain'
+            });
+
+            res.end(favoritos);
+          });
+
+        } else {
+          
+          //TODO incluir o valor no array apenas se ainda nao estiver no array
+          favorites.dishes.push(req.body._id);
+
+          favorites.save(function (err, favorites) {
+              if (err) throw err;
+              console.log('Updated Comments!');
+              res.json(favorites);
+          });
+        }
+      })
+});
+/*
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Dishes.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+dishRouter.route('/:dishId')
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Dishes.findById(req.params.dishId)
+        .populate('comments.postedBy')
+        .exec(function (err, dish) {
+        if (err) throw err;
+        res.json(dish);
+    });
+})
+
+.put(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, dish) {
+        if (err) throw err;
+        res.json(dish);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+        Dishes.findByIdAndRemove(req.params.dishId, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+dishRouter.route('/:dishId/comments')
+.all(Verify.verifyOrdinaryUser)
+
+.get(function (req, res, next) {
+    Dishes.findById(req.params.dishId)
+        .populate('comments.postedBy')
+        .exec(function (err, dish) {
+        if (err) throw err;
+        res.json(dish.comments);
+    });
+})
+
+.post(function (req, res, next) {
+    Dishes.findById(req.params.dishId, function (err, dish) {
+        if (err) throw err;
+        req.body.postedBy = req.decoded._doc._id;
+        dish.comments.push(req.body);
+        dish.save(function (err, dish) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(dish);
+        });
+    });
+})
+
+.delete(Verify.verifyAdmin, function (req, res, next) {
+    Dishes.findById(req.params.dishId, function (err, dish) {
+        if (err) throw err;
+        for (var i = (dish.comments.length - 1); i >= 0; i--) {
+            dish.comments.id(dish.comments[i]._id).remove();
+        }
+        dish.save(function (err, result) {
+            if (err) throw err;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Deleted all comments!');
+        });
+    });
+});
+
+dishRouter.route('/:dishId/comments/:commentId')
+.all(Verify.verifyOrdinaryUser)
+
+.get(function (req, res, next) {
+    Dishes.findById(req.params.dishId)
+        .populate('comments.postedBy')
+        .exec(function (err, dish) {
+        if (err) throw err;
+        res.json(dish.comments.id(req.params.commentId));
+    });
+})
+
+.put(function (req, res, next) {
+    // We delete the existing commment and insert the updated
+    // comment as a new comment
+    Dishes.findById(req.params.dishId, function (err, dish) {
+        if (err) throw err;
+        dish.comments.id(req.params.commentId).remove();
+                req.body.postedBy = req.decoded._doc._id;
+        dish.comments.push(req.body);
+        dish.save(function (err, dish) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(dish);
+        });
+    });
+})
+
+.delete(function (req, res, next) {
+    Dishes.findById(req.params.dishId, function (err, dish) {
+        if (dish.comments.id(req.params.commentId).postedBy
+           != req.decoded._doc._id) {
+            var err = new Error('You are not authorized to perform this operation!');
+            err.status = 403;
+            return next(err);
+        }
+        dish.comments.id(req.params.commentId).remove();
+        dish.save(function (err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
+    });
+});
+*/
+module.exports = favoritesRouter;
