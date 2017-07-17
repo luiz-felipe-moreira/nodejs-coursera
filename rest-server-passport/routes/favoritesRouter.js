6063ed5cc56bf42af9835d34c2cd811c353b79f4
Luiz Felipe Sampaio Castelo Branco Moreira - SUPDE/DESDR/DE5ED <82586004515@serpro-1455424.(none)>
2017-07-17 12:01:00 -0300

    ajustando o post em favoritos para não  duplicar dishIds

--- rest-server-passport/routes/favoritesRouter.js
+++ rest-server-passport/routes/favoritesRouter.js
@@ -9,23 +9,23 @@ var favoritesRouter = express.Router();
 favoritesRouter.use(bodyParser.json());
 
 favoritesRouter.route('/')
-  .get(Verify.verifyOrdinaryUser, function(req, res, next) {
+  .get(Verify.verifyOrdinaryUser, function (req, res, next) {
     Favorites.findOne({
-        postedBy: req.decoded._doc._id
-      })
+      postedBy: req.decoded._doc._id
+    })
       .populate('postedBy')
       .populate('dishes')
-      .exec(function(err, favorites) {
+      .exec(function (err, favorites) {
         if (err) throw err;
         res.json(favorites);
       });
   })
 
-  .post(Verify.verifyOrdinaryUser, function(req, res, next) {
+  .post(Verify.verifyOrdinaryUser, function (req, res, next) {
     Favorites.findOne({
-        postedBy: req.decoded._doc._id
-      })
-      .exec(function(err, favorites) {
+      postedBy: req.decoded._doc._id
+    })
+      .exec(function (err, favorites) {
         if (err) throw err;
 
         if (favorites === null) {
@@ -36,58 +36,59 @@ favoritesRouter.route('/')
           favoritos.postedBy = req.decoded._doc._id;
           favoritos.dishes.push(req.body._id)
 
-          Favorites.create(favoritos, function(err, favorites) {
+          Favorites.create(favoritos, function (err, favorites) {
             if (err) throw err;
             console.log('Favorites created!');
             res.writeHead(200, {
               'Content-Type': 'text/plain'
             });
 
-            res.end(favoritos);
+            res.json(favoritos);
           });
 
         } else {
 
-          //TODO incluir o valor no array apenas se ainda nao estiver no array
-          favorites.dishes.push(req.body._id);
-
-          favorites.save(function(err, favorites) {
-            if (err) throw err;
-            console.log('Updated Comments!');
+          if (favorites.dishes.indexOf(req.body._id) >= 0) {
             res.json(favorites);
-          });
+          } else {
+            favorites.dishes.push(req.body._id);
+
+            favorites.save(function (err, favorites) {
+              if (err) throw err;
+              console.log('Updated Comments!');
+              res.json(favorites);
+            });
+          }
         }
       })
   })
-  .delete(Verify.verifyOrdinaryUser, function(req, res, next) {
+  .delete(Verify.verifyOrdinaryUser, function (req, res, next) {
     Favorites.remove({
       postedBy: req.decoded._doc._id
-    }, function(err, resp) {
+    }, function (err, resp) {
       if (err) throw err;
       res.json(resp);
     });
   });
 
 favoritesRouter.route('/:dishId')
-  .delete(Verify.verifyOrdinaryUser, function(req, res, next) {
-    // remove the specified dish from the list of the user's favorite dishes
-    console.log('iniciando o processamento da requisição delete');
+  .delete(Verify.verifyOrdinaryUser, function (req, res, next) {
 
     Favorites.findOne({
       postedBy: req.decoded._doc._id
     })
-    .exec(function(err, favorites) {
-      if (err) throw err;
-      for (var i = (favorites.dishes.length - 1); i >= 0; i--) {
-        if (favorites.dishes[i] == req.params.dishId) {
-          favorites.dishes.splice(i, 1);
-        }
-      }
-      favorites.save(function(err, resp) {
+      .exec(function (err, favorites) {
         if (err) throw err;
-        res.json(resp);
+        for (var i = (favorites.dishes.length - 1); i >= 0; i--) {
+          if (favorites.dishes[i] == req.params.dishId) {
+            favorites.dishes.splice(i, 1);
+          }
+        }
+        favorites.save(function (err, resp) {
+          if (err) throw err;
+          res.json(resp);
+        });
       });
-    });
   });
 
 module.exports = favoritesRouter;
